33fbddb7330e59bbeec67ccde4572df858002040
Harsh Singh <harshsinghkumwar97819@gmail.com>
2026-07-04 22:21:19 +0530

    note added

---
+++ script.js
@@ -0,0 +1,42 @@
+const notesContainer = document.querySelector(".notes-container")
+const addNote = document.querySelector("#add_note")
+const modal = document.querySelector(".modal")
+const cancelBtn = document.querySelector("#cancel") ;
+const saveBtn = document.querySelector("#save") ;
+const titleInput = document.querySelector("#title")
+const contentInput = document.querySelector("#content") ;
+
+addNote.addEventListener('click',function() {
+modal.classList.remove("hidden");
+
+})
+
+saveBtn.addEventListener('click',function() {
+    const note = document.createElement("article")
+const title = document.createElement("h3")
+const paragraph = document.createElement("p")
+const edit = document.createElement("button")
+const del = document.createElement("button")
+const copy = document.createElement("button") 
+paragraph.textContent = "Buy Milk" ;
+edit.textContent = "Edit" ;
+del.textContent = "Delete" ;
+copy.textContent = "Copy" ;
+// title.textContent = "Title" ;
+title.textContent =  titleInput.value ;
+paragraph.textContent = contentInput.value ;
+const container = document.createElement("div") ;
+container.appendChild(edit) ;
+container.appendChild(del) ;
+container.appendChild(copy) ;
+note.appendChild(title) ;
+note.appendChild(paragraph) ;
+note.classList.add('note') ;
+container.classList.add('actions') ;
+note.appendChild(container) ;
+notesContainer.appendChild(note) ;
+modal.classList.add("hidden") ;
+})
+cancelBtn.addEventListener('click',function() {
+    modal.classList.add('hidden') ;
+})
